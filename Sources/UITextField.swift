d5be91e309984bb6b7d9e4fbab58bceb73261126
Colin Reisterer <creisterer@starry.com>
2016-07-08 10:26:01 -0400

    Only send changed values to rText and rAttributedText

--- Sources/UITextField.swift
+++ Sources/UITextField.swift
@@ -51,9 +51,12 @@ extension UITextField {
         .filter { $0 == UIControlEvents.AllEditingEvents }
         .observeNext { [weak self] event in
           guard let unwrappedSelf = self else { return }
-          updatingFromSelf = true
-          unwrappedSelf.rText.value = unwrappedSelf.text
-          updatingFromSelf = false
+          // only send to rText if value changed, as .AllEditingEvents reports more than just changes
+          if unwrappedSelf.rText.value != unwrappedSelf.text {
+            updatingFromSelf = true
+            unwrappedSelf.rText.value = unwrappedSelf.text
+            updatingFromSelf = false
+          }
         }.disposeIn(rBag)
       
       return rText
@@ -79,9 +82,12 @@ extension UITextField {
         .filter { $0 == UIControlEvents.AllEditingEvents }
         .observeNext { [weak self] event in
           guard let unwrappedSelf = self else { return }
-          updatingFromSelf = true
-          unwrappedSelf.rAttributedText.value = unwrappedSelf.attributedText
-          updatingFromSelf = false
+          // only sent to rAttributedText if value changed, as .AllEditingEvents reports more than just changes
+          if unwrappedSelf.rAttributedText.value != unwrappedSelf.attributedText {
+            updatingFromSelf = true
+            unwrappedSelf.rAttributedText.value = unwrappedSelf.attributedText
+            updatingFromSelf = false
+          }
         }.disposeIn(rBag)
       
       return rAttributedText
